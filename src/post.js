d1c1b18ac3131b700a2038bf6699bc3e10421a5e
nundung <jjy809123@naver.com>
2024-03-04 15:37:07 +0900

    Add: feature브랜치 생성

---
+++ src/post.js
@@ -0,0 +1,13 @@
+Router.post(
+    '/',
+    isToken, async (req, res, next) => {
+        const { title, content, image_link } = req.body;
+        try {
+            const sql = 
+        }
+        catch {
+            
+        }
+    }
+
+)
